40ae1debcf38aeff0acfb96838471f83ace8f1c6
ALangford98 <namibia.langford@gmail.com>
2023-08-29 12:57:24 +0200

    Added a mock API to display some content on Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,52 @@
 import React from 'react'
 
+const mockProducts = [
+  {
+    id: 1,
+    title: 'Product 1',
+    description: 'Description for Product 1',
+    price: 19.99,
+    imageUrl: 'https://pngimg.com/uploads/milk/milk_PNG12696.png'
+  },
+  {
+    id: 2,
+    title: 'Product 2',
+    description: 'Description for Product 2',
+    price: 29.99,
+    imageUrl: 'https://pngimg.com/uploads/bread/bread_PNG2224.png'
+  },
+  {
+    id: 3,
+    title: 'Product 3',
+    description: 'Description for Product 3',
+    price: 39.99,
+    imageUrl: 'https://img.freepik.com/premium-psd/cigarette-packet-mockup_47987-2694.jpg?w=740'
+  }
+];
+
 function Home() {
   return (
     <div className="parent-container">
       <div className="scrollable-div">
         <div className='Home'>
           <h1>Featured Products</h1>
-            <div className='ItemList'>
-              <div className='Card' ></div>
-              <div className='Card' ></div>
-              <div className='Card' ></div>
-          </div>
-            <h1 className='CategoryHead'>Browse Categories</h1>
           <div className='ItemList'>
-            <div className='TableCard'></div>
+            {mockProducts.map(product => (
+              <div className='Card' key={product.id}>
+                <div className='Image'>
+                  <img src={product.imageUrl} alt={product.title} />
+                </div>
+                <div className='Title'>{product.title}</div>
+                <div className='Description'>{product.description}</div>
+                <p className='Price'>Price: ${product.price}</p>
+              </div>
+            ))}
           </div>
+          {/* ... other elements */}
         </div>
       </div>
     </div>
-
-  )
+  );
 }
 
 export default Home
